f61dec9ca1d948082250c8d8c15a4904465bde84
zolotarevayaa <152485962+zolotarevayaa@users.noreply.github.com>
2023-11-30 14:50:58 +0500

    Площадь и периметр прямоугольника, арифметический подсчет числа

---
+++ ДЗ4-1.py
@@ -0,0 +1,4 @@
+a = float(input())
+b = float(input())
+print(a*b)
+print(2*(a+b))
---
+++ ДЗ4-2.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+print((d**e)*c/(a-b))
